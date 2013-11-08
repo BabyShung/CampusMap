011b0f8349108baa8f8ff1977ecc3a1f7e6531e5
BabyShung <hao-zheng@uiowa.edu>
2013-11-07 21:40:53 -0600

    hao modified 3

--- src/com/example/campusmap/BuildingDrawing.java
+++ src/com/example/campusmap/BuildingDrawing.java
@@ -11,13 +11,13 @@ import com.google.android.gms.maps.model.PolygonOptions;
 
 public class BuildingDrawing {
 
-	private Map<Integer,Building> buildingSet;
+	private Map<Integer, Building> buildingSet;
 	private GoogleMap map;
 	private Building currentTouchedBuilding;
 
 	public BuildingDrawing(GoogleMap map) {
- 
-		buildingSet = new HashMap<Integer,Building >();
+
+		buildingSet = new HashMap<Integer, Building>();
 		this.map = map;
 		drawAllTheCampusBuildings();
 	}
@@ -28,9 +28,9 @@ public class BuildingDrawing {
 		int j;
 		boolean result = false;
 		for (Building b : buildingSet.values()) {
-			//each time, we take one points-set from the hashmap
+			// each time, we take one points-set from the hashmap
 			LatLng[] bp = b.getPoints();
-			//algorithm for detecting a point in a polygon
+			// algorithm for detecting a point in a polygon
 			for (i = 0, j = bp.length - 1; i < bp.length; j = i++) {
 				if ((bp[i].longitude > touchPoint.longitude) != (bp[j].longitude > touchPoint.longitude)
 						&& (touchPoint.latitude < (bp[j].latitude - bp[i].latitude)
@@ -49,46 +49,54 @@ public class BuildingDrawing {
 		return result;
 	}
 
-	public Building getCurrentTouchedBuilding(){
+	public Building getCurrentTouchedBuilding() {
 		return currentTouchedBuilding;
 	}
-	
+
 	private void drawAllTheCampusBuildings() {
-		
-		buildingSet.put(1, new Building("Meclean Hall","address1",new LatLng(41.661017, -91.536656), new LatLng(41.661017,
-				-91.536420), new LatLng(41.660418, -91.536420), new LatLng(
-				41.660418, -91.536656)));
-		buildingSet.put(2, new Building("Jessup Hall","address2",new LatLng(41.662203, -91.536628), new LatLng(41.662203,
-				-91.536393), new LatLng(41.661598, -91.536400), new LatLng(
-				41.661598, -91.536635)));
-//		buildingSet.put(2, new Building("Main Library","address3",new LatLng(41.659932, -91.538974), new LatLng(41.659932,
-//				-91.537903), new LatLng(41.659144, -91.537903), new LatLng(
-//				41.659144, -91.538974)));
- 
+
+		buildingSet.put(1, new Building("Meclean Hall", "address1", new LatLng(
+				41.661017, -91.536656), new LatLng(41.661017, -91.536420),
+				new LatLng(41.660418, -91.536420), new LatLng(41.660418,
+						-91.536656)));
+		buildingSet.put(2, new Building("Jessup Hall", "address2", new LatLng(
+				41.662203, -91.536628), new LatLng(41.662203, -91.536393),
+				new LatLng(41.661598, -91.536400), new LatLng(41.661598,
+						-91.536635)));
+		buildingSet.put(2, new Building("Main Library", "address3", new LatLng(
+				41.659932, -91.538974), new LatLng(41.659932, -91.537903),
+				new LatLng(41.659144, -91.537903), new LatLng(41.659144,
+						-91.538167), new LatLng(41.659108, -91.538167),
+				new LatLng(41.659108, -91.538680),new LatLng(41.659144, -91.538680), new LatLng(41.659144,
+						-91.538974)));
+
 	}
 
 	public class Building {
-		
+
 		private String buildingName;
 		private LatLng[] points;
 		private String address;
 
-		public Building(String buildingName, String address,LatLng...points) {
+		public Building(String buildingName, String address, LatLng... points) {
 			this.buildingName = buildingName;
 			this.points = points;
 			this.address = address;
 			this.drawPloygon();
 		}
 
-		public String getBuildingName(){
+		public String getBuildingName() {
 			return buildingName;
 		}
-		public LatLng[] getPoints(){
+
+		public LatLng[] getPoints() {
 			return points;
 		}
-		public String getAddress(){
+
+		public String getAddress() {
 			return address;
 		}
+
 		private void drawPloygon() {
 
 			PolygonOptions po = new PolygonOptions();
@@ -98,6 +106,6 @@ public class BuildingDrawing {
 			po.strokeColor(Color.RED).fillColor(Color.BLUE);
 			map.addPolygon(po);
 		}
-		
+
 	}
 }
